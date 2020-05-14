dbaf34352d16d36683d827efbcfb20f30174dfd7
Gavin Vazquez <gavinvazquez@Gavins-MacBook-Air.local>
2020-05-13 23:47:47 -0400

    added bank.cpp

---
+++ bank.cpp
@@ -0,0 +1,78 @@
+#include "Customer.h"
+#include "Account.h"
+#include "functions.cpp"
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+
+int main(){
+    Node *head;
+    vector<Customer>allCustomers;
+    string loginName;
+    string loginPassword;
+    string first;
+    string last;
+
+    int option = 0;
+    bool loopStop = false;
+    while(loopStop != true){
+        cout<<"\n\tMAIN MENU";
+        cout<<"\n\n\t1. NEW ACCOUNT";
+        cout<<"\n\n\t2. DEPOSIT AMOUNT";
+        cout<<"\n\n\t3. WITHDRAW AMOUNT";
+        cout<<"\n\n\t4. BALANCE ENQUIRY";
+        cout<<"\n\n\t5. ALL ACCOUNT HOLDER LIST";
+        cout<<"\n\n\t6. CLOSE AN ACCOUNT";
+        cout<<"\n\n\t7. MODIFY AN ACCOUNT";
+        cout<<"\n\n\t8. EXIT";
+        cout<<"\n\n";
+        cout<<"\n\n\tSelect Your Option: ";
+        cin>>option;
+
+        switch(option)
+        {
+        case 1:
+            createProfile(allCustomers);
+            break;
+
+        case 2:
+            depositAmount(allCustomers);
+            break;
+
+        case 3:
+            withdrawAmount(allCustomers);
+            break;
+
+        case 4:
+            checkEnquiry(allCustomers);
+            break;
+
+        case 5:
+            displayAccounts(allCustomers);
+            break;
+
+        case 6:
+            closeAccount(allCustomers);
+            break;
+
+        case 7:
+            editAccount(allCustomers);
+            break;
+
+        case 8:
+            cout<<"Thank you for using the bank managment service!";
+            loopStop = true;
+            cout<<"\n\n";
+            return 0;
+            break;
+        
+        default:{
+            break;
+        }
+      }
+    }
+    return 0;
+}
+
